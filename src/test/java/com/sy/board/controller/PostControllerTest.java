beb4c40ade3092cbed3d0c0f450cd7ad7a43c820
sendkite <sendkite1@gmail.com>
2022-06-20 00:07:58 +0900

    project init

---
+++ src/test/java/com/sy/board/controller/PostControllerTest.java
@@ -0,0 +1,27 @@
+package com.sy.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청시 hello world")
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello world"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+}
